e37e861dbacaa13d764223a0641e5fa85759a8a3
emfy <moonmeander47@ya.ru>
2022-05-08 02:11:01 +0300

    added colored output

--- lib/console_interface.rb
+++ lib/console_interface.rb
@@ -1,3 +1,5 @@
+require 'colorize'
+
 class ConsoleInterface
   # В константе FIGURES будут лежать все текстовые файлы из папки figures,
   # помещённые в массив. Один элемент массива — одна строка с содержимым целого
@@ -19,12 +21,14 @@ class ConsoleInterface
   # Выводит в консоль текущее состояние игры, используя данные из экземпляра
   # класса Game (количество ошибок, сколько осталось попыток и т.д.)
   def print_out
-    puts <<~END
-      Слово: #{word_to_show}
-      #{figure}
-      Ошибки (#{@game.errors_made}): #{errors_to_show}
-      У вас осталось ошибок: #{@game.errors_allowed}
+    word = "Слово: #{word_to_show}".colorize(:blue)
+    errors = "Ошибки (#{@game.errors_made}): #{errors_to_show}".colorize(:red)
 
+    puts <<~END
+      #{word}
+      #{figure}
+      #{errors}
+      У вас осталось ошибок: #{@game.errors_allowed}
     END
 
     if @game.won?
